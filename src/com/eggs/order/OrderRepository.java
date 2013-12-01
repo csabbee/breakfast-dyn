7370506aa2929247d3fd4171c653f798696f961f
Csaba Valyi <csaba.valy1@gmail.com>
2013-12-01 11:51:20 +0100

    [fix] removeOrderInstance fixed

--- src/com/eggs/order/OrderRepository.java
+++ src/com/eggs/order/OrderRepository.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class OrderRepository {
-
+    
     private Map<String, OrderInstance> orders = new HashMap<>();
     private int id = 0;
     
@@ -14,6 +14,14 @@ public class OrderRepository {
     }
     public void removeOrderInstance(String id){
         orders.remove(id);
+        int index = Integer.parseInt(id);
+        for (int i = index+1; i < this.id; i++) {
+            orders.get(Integer.toString(i)).setId(i-1);
+            orders.put(Integer.toString(i-1), orders.get(Integer.toString(i)));
+            orders.remove(Integer.toString(i));
+        }
+        orders.remove(this.id);
+        if(this.id > 0) this.id--;
     }
     public Map<String, OrderInstance> getOrderInstances(){
         Map<String, OrderInstance> orderInstances = new HashMap<>(orders);
