16d1f02a6c24ab75fc631beb857cf83544a3ae35
Csaba Valyi <csaba.valy1@gmail.com>
2013-12-01 14:12:33 +0100

    YamlFileFoodRepository read method implemented properly

--- src/com/eggs/domain/YamlFileFoodRepository.java
+++ src/com/eggs/domain/YamlFileFoodRepository.java
@@ -1,16 +1,30 @@
 package com.eggs.domain;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.yaml.snakeyaml.TypeDescription;
+import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
 public class YamlFileFoodRepository {
 
     private List<Food> initfoods = new ArrayList<>();
     
-    public void read(){
+    public List<Food> read(InputStream stream){
         Constructor constructor = new Constructor(Food.class);
+        TypeDescription foodDescription = new TypeDescription(Food.class);
+        constructor.addTypeDescription(foodDescription);
+        Yaml yaml = new Yaml(constructor);
+        Iterable<Object> iter = yaml.loadAll(stream);
         
+        for(Object o : iter){
+            addFood((Food)o);
+         }
+        return initfoods;
+    }
+    public void addFood(Food food){
+        initfoods.add(food);
     }
 }
